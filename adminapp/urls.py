298d56c97087ea5a1f1b75feb2caf17701510f8f
sujith132 <sujithsubash2345@gmail.com>
2024-07-11 18:14:41 +0530

    admin user management(users list, block, ublock)

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     path('admin_login/', views.admin_login, name='admin_login'),
     path('admin_logout/',views.admin_logout , name='admin_logout'),
     
+    path('userslist/', views.list_users, name='admin_user_list'),
+    path('users/block/<int:user_id>/', views.block_user, name='block_user'),
+    path('unblock_user/<int:user_id>/', views.unblock_user, name='unblock_user'),
     
-    path('usermanagement/',views.admin_userManagement, name='user_management'),
+    
+    
     path('productmanagement/',views.admin_productManagement, name='productmanagement'),
     path('categorymanagement/',views.admin_categoryManagement, name='categorymanagement'),
     path('ordermanagement/',views.admin_orderManagement, name='ordermanagement'),
--- adminapp/views.py
+++ adminapp/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required,user_passes_test
 from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 
 
 
@@ -33,10 +35,43 @@ def admin_dashboard(request):
     return render(request, 'admin_signin.html')
 
 
+# User management section
+
 @staff_member_required(login_url='admin_login')
 @login_required(login_url='admin_login')
-def admin_userManagement(request):
-    return render(request, 'admin_usermanagement.html')
+@staff_member_required
+def list_users(request):
+    query = request.GET.get('search')
+    if query: 
+        users = User.objects.filter(username__icontains=query) | User.objects.filter(email__icontains=query)
+        
+    else:
+        users = User.objects.all()
+        users = users.order_by('username')
+    return render(request, 'admin_usermanagement.html', {'users': users})
+
+@staff_member_required
+def block_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if user.is_superuser:
+        messages.error(request, "You cannot block a superuser.")
+        
+    else:
+        user.is_active = False
+        user.save()
+        messages.success(request, "User has been blocked successfully.")
+    return redirect('admin_user_list')
+@staff_member_required
+def unblock_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.is_active = True
+    user.save()
+    messages.success(request, "User has been unblocked successfully.")
+    return redirect('admin_user_list')
+
+
+
+
 
 
 
