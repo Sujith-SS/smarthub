181971d8150c49abdea3036f69a1a2289a9ee8a0
sujith132 <sujithsubash2345@gmail.com>
2024-07-11 11:58:02 +0530

    admin login, logout

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('administration', views.admin_login, name='administration'),
+    path('administration', views.admin_dashboard, name='administration'),
+    path('admin_login/', views.admin_login, name='admin_login'),
+    path('admin_logout/',views.admin_logout , name='admin_logout'),
+    
+    
+    path('usermanagement/',views.admin_userManagement, name='user_management'),
+    path('productmanagement/',views.admin_productManagement, name='productmanagement'),
+    path('categorymanagement/',views.admin_categoryManagement, name='categorymanagement'),
+    path('ordermanagement/',views.admin_orderManagement, name='ordermanagement'),
+    path('salesreport/',views.admin_salesRepot, name='salesreport'),
+    
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.admin.views.decorators import staff_member_required
 
-# Create your views here.
+
 
 def admin_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            if user.is_superuser:
+                return redirect('administration')
+        else:
+            messages.error(request, 'Invalid username or password.')
     return render(request, 'admin_signin.html')
+
+
+def admin_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('admin_login')
+ 
+
+def admin_dashboard(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return render(request, 'admin_dashboard.html')
+    return render(request, 'admin_signin.html')
+
+
+@staff_member_required(login_url='admin_login')
+@login_required(login_url='admin_login')
+def admin_userManagement(request):
+    return render(request, 'admin_usermanagement.html')
+
+
+
+def admin_productManagement(request):
+    return render(request,'admin_productmanagement.html')
+
+
+
+def admin_categoryManagement(request):
+    return render(request,'admin_categorymanagement.html')
+
+
+def admin_orderManagement(request):
+    return render (request, 'admin_ordermanagement.html')
+
+def admin_salesRepot(request):
+    return render (request, 'sales_report.html')
+
+
